019159d50e533bc99bf9b1960e96efe065b61fcb
swarup4 <swarup.saha004@hotmail.com>
2019-02-27 02:07:47 +0530

    Add New Component

---
+++ cart/src/app/apiConstants.ts
@@ -0,0 +1,4 @@
+export class Api {
+    public static userApi = 'http://127.0.0.1:3000/api/user';
+    public static orderApi = 'http://127.0.0.1:3000/api/order';
+}
--- cart/src/app/app.module.ts
+++ cart/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { OrderComponent } from './order/order.component';
+import { ProductsComponent } from './products/products.component';
+
+import { LoginService } from './login/login.service';
+import { HomeService } from './home/home.service';
+import { ProductsService } from './products/products.service';
+import { OrderService } from './order/order.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    OrderComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LoginService, HomeService, ProductsService, OrderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
